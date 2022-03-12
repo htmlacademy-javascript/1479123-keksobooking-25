a7cba833284e3717fc2340ae3ef5c5b43056fe6e
AlexDomanski <alex.domanskyi@gmail.com>
2022-03-12 17:08:15 +0100

    Генератор рандомного числа с плавающей точкой

---
+++ js/messages.js
@@ -0,0 +1,4 @@
+export const FIRST_VALUE_ERROR = 'First is not a number!';
+export const SECOND_VALUE_ERROR = 'Second is not a number!';
+export const RANGE_ERROR = 'First must be less than second!';
+export const DIGITS_ERROR = 'You need digits between 0 and 20!';
